4d404360d8bd3c238b36936870a8d029bfaffa4c
Priscilla <priscilla.gzz114@gmail.com>
2022-05-26 17:53:22 -0500

    adjusted format for readability, added copy button

--- script.js
+++ script.js
@@ -3,6 +3,14 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var el_down = document.getElementById("geeks");
+          function gfg_Run() {
+            el_down.innerHTML = 
+                Math.random().toString(36).slice(2) + 
+                Math.random().toString(36)
+                    .toUpperCase().slice(2);
+            } 
+
 
 // Write password to the #password input
 function writePassword() {
